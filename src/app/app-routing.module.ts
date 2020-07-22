7b7809a13333c3e0ea473b7c86e8a2a0784306bb
unknown <harshitwadhera20@gmail.com>
2020-07-23 02:38:56 +0530

    Skills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"about",component:AboutComponent},
+  {path:"skills",component:SkillsComponent},
 ];
 
 @NgModule({
