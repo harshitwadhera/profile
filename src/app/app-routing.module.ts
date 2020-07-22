95fb8ce94290f4c2b335387ab854b8573e6d2a90
unknown <harshitwadhera20@gmail.com>
2020-07-23 03:32:37 +0530

    education

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"about",component:AboutComponent},
   {path:"skills",component:SkillsComponent},
+  {path:"education",component:EducationComponent},
 ];
 
 @NgModule({
